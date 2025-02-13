d62b9f5815558640ff87ffee319a4e30987c5360
Linggar Maretva Cendani <linggarmc@gmail.com>
2025-02-13 18:52:00 +0700

    Add CSRF

--- manajemen_buku/settings.py
+++ manajemen_buku/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-# CSRF_TRUSTED_ORIGINS= [""]
+CSRF_TRUSTED_ORIGINS= ["https://manajemen-buku-kategori-production.up.railway.app"]
 
 
 # Quick-start development settings - unsuitable for production
